6d60930e6750a42f21f4a43d6c424a647da95461
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-10-16 17:11:42 +0000

    Snapshot of branch master taken on 2019-10-16T13:11:41.753-04:00 from "https://github.com/UnofficialJuliaMirror/WebAssembly.jl-4f6df8df-4739-5653-a877-cf57ee625c27"

--- src/ir.jl
+++ src/ir.jl
@@ -1,13 +1,15 @@
 using IRTools
-using IRTools: IR, Variable, isexpr, stmt, argument!, return!, xcall, block!,
-  branch!, blocks, insertafter!, arguments, argtypes, isreturn
+using IRTools: IR, CFG, Variable, isexpr, stmt, argument!, return!, xcall, block!,
+  branch!, blocks, insertafter!, arguments, argtypes, isreturn, stackify, isconditional
 
 function locals!(ir::IR)
   locals = argtypes(ir)
   env = Dict{Any,Any}(x => Local(i-1) for (i, x) in enumerate(arguments(ir)))
   rename(x::Variable) = env[x]
   rename(x::Real) = Const(x)
-  local!(v, T) = (push!(locals, T); env[v] = Local(length(locals)-1))
+  ltype(x) = rename(x) isa Const ? rename(x).typ : locals[rename(x).id+1]
+  local!(v, T) = haskey(env, v) ? env[v] :
+    (push!(locals, T); env[v] = Local(length(locals)-1))
   for b in blocks(ir)
     for (v, st) in b
       isexpr(st.expr) || (delete!(ir, v); env[v] = Const(st.expr); continue)
@@ -22,6 +24,12 @@ function locals!(ir::IR)
         push!(b, env[arguments(br)[1]])
         push!(b, Return())
       else
+        for (x, y) in zip(arguments(br), arguments(IRTools.block(ir, br.block)))
+          push!(b, rename(x))
+          push!(b, SetLocal(false, local!(y, ltype(x)).id))
+        end
+        isconditional(br) && push!(b, rename(br.condition))
+        push!(b, Branch(isconditional(br), br.block))
       end
     end
     empty!(IRTools.branches(b))
@@ -29,14 +37,41 @@ function locals!(ir::IR)
   return ir, locals
 end
 
-function reloop(ir)
-  Block([st.expr for (v, st) in ir])
+isbackedge((from, to)) = to <= from
+
+function reloop(ir, cfg)
+  scopes = []
+  targets = []
+  forw, back = stackify(cfg)
+  push!(scopes, Block([]))
+  block!() = (bl = Block([]); push!(scopes[1].body, bl); pushfirst!(scopes, bl))
+  loop!() = (bl = Loop([]); push!(scopes[1].body, bl); pushfirst!(scopes, bl))
+  for b in blocks(ir)
+    any(br -> br[2] == b.id, forw) && (popfirst!(scopes); popfirst!(targets))
+    brs = vcat(filter(br -> br[1] == b.id, forw), filter(br -> br[2] == b.id, back))
+    sort!(brs, by = br -> isbackedge(br) ? (br[1], 1) : (br[2], -1), rev = true)
+    for br in brs
+      isbackedge(br) ? loop!() : block!()
+      pushfirst!(targets, br[2])
+    end
+    for (v, st) in b
+      if st.expr isa Branch
+        st.expr.cond && push!(scopes[1].body, i32.eqz)
+        target = findfirst(b -> b == st.expr.level, targets)-1
+        push!(scopes[1].body, Branch(st.expr.cond, target))
+      else
+        push!(scopes[1].body, st.expr)
+      end
+    end
+    any(br -> br[1] == b.id, back) && (popfirst!(scopes); popfirst!(targets))
+  end
+  return scopes[end]
 end
 
 function irfunc(name, ir)
+  cfg = CFG(ir)
   ir, locals = locals!(ir)
   params = locals[1:length(arguments(ir))]
   locals = locals[length(arguments(ir))+1:end]
-  params, locals
-  Func(name, params, [f64], locals, reloop(ir))
+  Func(name, params, [f64], locals, reloop(ir, cfg))
 end
--- test/ir.jl
+++ test/ir.jl
@@ -9,7 +9,7 @@ add = let
   return!(ir, c)
 end
 
-func = irfunc(:foo, copy(add))
+func = irfunc(:add, copy(add))
 
 @test WebAssembly.interpretwasm(func, Dict(), [2, 3]) == [5.0]
 
@@ -25,19 +25,27 @@ relu = let
   ir
 end
 
+func = irfunc(:relu, copy(relu))
+
+@test WebAssembly.interpretwasm(func, Dict(), [5]) == [5]
+@test WebAssembly.interpretwasm(func, Dict(), [-5]) == [0]
+
 pow = let
   ir = IR()
   x = argument!(ir, f64)
-  n = argument!(ir, i64)
+  n = argument!(ir, i32)
   cond = block!(ir)
   body = block!(ir)
   ret  = block!(ir)
   n = argument!(cond, n)
-  r = argument!(cond, 0.0)
-  branch!(cond, ret, unless = push!(cond, stmt(xcall(i64.gt, n, 0), type = i32)))
-  n = push!(body, stmt(xcall(i64.sub, n, 1), type = i64))
+  r = argument!(cond, 1.0)
+  branch!(cond, ret, unless = push!(cond, stmt(xcall(i32.gt_s, n, Int32(0)), type = i32)))
+  n = push!(body, stmt(xcall(i32.sub, n, Int32(1)), type = i32))
   r′ = push!(body, stmt(xcall(f64.mul, r, x), type = f64))
   branch!(body, cond, n, r′)
   return!(ret, r)
   ir
 end
+
+func = irfunc(:pow, copy(pow))
+@test WebAssembly.interpretwasm(func, Dict(), [2, 3]) == [8]
